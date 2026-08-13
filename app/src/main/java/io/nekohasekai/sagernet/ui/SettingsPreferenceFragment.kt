5c3c6b1d27b88090a8f1fc136c99c286aa55a143
Ajizzz13 <zevpnstore@gmail.com>
2026-08-13 11:00:34 +0000

    feat: permanently remove features from settings menu to avoid phantom popups, keeping them exclusively in the main menu

--- app/src/main/java/io/nekohasekai/sagernet/ui/SettingsPreferenceFragment.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/SettingsPreferenceFragment.kt
@@ -194,8 +194,7 @@ class SettingsPreferenceFragment : PreferenceFragmentCompat() {
         appendHttpProxy.onPreferenceChangeListener = reloadListener
         showDirectSpeed.onPreferenceChangeListener = reloadListener
         trafficSniffing.onPreferenceChangeListener = reloadListener
-        enableTlsFragment.onPreferenceChangeListener = reloadListener
-        findPreference<SwitchPreference>("enable_game_auto_stabilizer")!!.onPreferenceChangeListener = reloadListener
+
         bypassLan.onPreferenceChangeListener = reloadListener
         bypassLanInCore.onPreferenceChangeListener = reloadListener
         mtu.onPreferenceChangeListener = reloadListener
