4b1c88ae2d353f7d4ca2d4d478f37f535c4271e8
Ajizzz13 <zevpnstore@gmail.com>
2026-08-13 10:13:47 +0000

    fix: correctly check DataStore instead of SharedPreferences to prevent phantom apply popup

--- app/src/main/java/io/nekohasekai/sagernet/ui/SettingsPreferenceFragment.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/SettingsPreferenceFragment.kt
@@ -33,9 +33,9 @@ class SettingsPreferenceFragment : PreferenceFragmentCompat() {
 
     private val reloadListener = Preference.OnPreferenceChangeListener { pref, newValue ->
         val isChanged = when (pref) {
-            is SwitchPreference -> pref.sharedPreferences?.getBoolean(pref.key, pref.isChecked) != (newValue as? Boolean)
-            is ListPreference -> pref.sharedPreferences?.getString(pref.key, pref.value) != (newValue as? String)
-            is EditTextPreference -> pref.sharedPreferences?.getString(pref.key, pref.text) != (newValue as? String)
+            is SwitchPreference -> DataStore.configurationStore.getBoolean(pref.key, pref.isChecked) != (newValue as? Boolean)
+            is ListPreference -> DataStore.configurationStore.getString(pref.key, pref.value) != (newValue as? String)
+            is EditTextPreference -> DataStore.configurationStore.getString(pref.key, pref.text) != (newValue as? String)
             else -> true
         }
         if (isChanged) {
